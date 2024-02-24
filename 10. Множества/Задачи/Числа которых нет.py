6590ac27ffc6b71db2d7bac9eb3d4775930d1145
BEN10 <nishtichek@gmail.com>
2024-02-24 15:54:43 +0300

    Решение задачи

--- 10. Множества/Задачи/Числа которых нет.py
+++ 10. Множества/Задачи/Числа которых нет.py
@@ -1,9 +1,15 @@
 nums = {'0','1','2','3','4','5','6','7','8','9'}
 s = input()
+s1 = ''
 setn = set()
 for i in s:
     if i.isalpha():
         s = s.replace(i,'')
 for i in s:
     setn.add(i)
-print(*(nums.difference(setn)))
+if len(nums.difference(setn)) == 0:
+    print('NO')
+else:
+    for i in nums.difference(setn):
+        s1 += str(i)
+    print(*(sorted(s1)[::-1]))
