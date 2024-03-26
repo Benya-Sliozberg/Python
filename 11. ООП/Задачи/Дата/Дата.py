6357fe00c39cd13a310ee7b5c5d1b42ae9134374
BEN10 <nishtichek@gmail.com>
2024-03-26 16:03:04 +0300

    Решение дз

---
+++ 11. ООП/Задачи/Дата/Дата.py
@@ -0,0 +1,44 @@
+class Date:
+    def __init__(self, date):
+        lst = date.split('.')
+        self.day = int(lst[0])
+        self.month = int(lst[1])
+        self.year = int(lst[2])
+
+    def add_day(self):
+        if self.year % 4 == 0 and self.year % 100 != 0 or self.year % 400 == 0:
+            if self.month == 2:
+                self.day += 1
+                if self.day > 29:
+                    self.day = 1
+                    self.month += 1
+        else:
+            if self.month == 2:
+                self.day += 1
+                if self.day > 28:
+                    self.day = 1
+                    self.month += 1
+            elif self.month == 12:
+                self.day += 1
+                if self.day > 31:
+                    self.day = 1
+                    self.month = 1
+                    self.year += 1
+            elif self.month != 4 and self.month != 6 and self.month != 9 and self.month != 11:
+                self.day += 1
+                if self.day > 31:
+                    self.day = 1
+                    self.month += 1
+            elif self.month == 4 and self.month == 6 and self.month == 9 and self.month == 11:
+                self.day += 1
+                if self.day > 30:
+                    self.day = 1
+                    self.month += 1
+
+    def __str__(self):
+        return f'{self.day}.{self.month}.{self.year}'
+
+
+benya = Date('31.1.2023')
+benya.add_day()
+print(benya)
