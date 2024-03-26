16a0a87c6f6a4bfdce824ef033a986e8e3e0da52
BEN10 <nishtichek@gmail.com>
2024-03-26 16:39:16 +0300

    Двойное Исправление решенного дз

--- 11. ООП/Задачи/Дата/Дата.py
+++ 11. ООП/Задачи/Дата/Дата.py
@@ -18,16 +18,18 @@ class Date:
                     self.day = 1
                     self.month = 1
                     self.year += 1
-            elif self.month != 4 or self.month != 6 or self.month != 9 or self.month != 11:
-                self.day += 1
-                if self.day > 31:
-                    self.day = 1
-                    self.month += 1
             elif self.month == 4 or self.month == 6 or self.month == 9 or self.month == 11:
                 self.day += 1
                 if self.day > 30:
                     self.day = 1
                     self.month += 1
+            else:
+                self.day += 1
+                if self.day > 31:
+                    self.day = 1
+                    self.month += 1
+
+
         else:
             if self.month == 2:
                 self.day += 1
@@ -40,21 +42,22 @@ class Date:
                     self.day = 1
                     self.month = 1
                     self.year += 1
-            elif self.month != 4 or self.month != 6 or self.month != 9 or self.month != 11:
-                self.day += 1
-                if self.day > 31:
-                    self.day = 1
-                    self.month += 1
             elif self.month == 4 or self.month == 6 or self.month == 9 or self.month == 11:
                 self.day += 1
                 if self.day > 30:
                     self.day = 1
                     self.month += 1
+            else:
+                self.day += 1
+                if self.day > 31:
+                    self.day = 1
+                    self.month += 1
+
 
     def __str__(self):
         return f'{self.day}.{self.month}.{self.year}'
 
 
-benya = Date('28.3.2000')
+benya = Date('30.6.2000')
 benya.add_day()
 print(benya)
