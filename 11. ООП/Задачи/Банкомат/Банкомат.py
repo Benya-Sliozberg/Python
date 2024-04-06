b3425951189be6bd801bbf442ffd9773708a9f4b
Андрей Боев <a.boev.it@gmail.com>
2024-04-06 15:42:48 +0300

    Проверка ДЗ и исправление

---
+++ 11. ООП/Задачи/Банкомат/Банкомат.py
@@ -0,0 +1,50 @@
+class ATM:
+    __max_cash_count = 1000
+    def __init__(self, id):
+        self.__cash = {5000: 0, 1000: 9, 500: 12, 100: 9, 50: 4}
+        self.__id = id
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, id):
+        if id < 0:
+            raise ValueError('Недопустимое значение')
+        self.__id = id
+
+    def total_amount(self):
+        total_sum = 0
+        for key, value in self.__cash.items():
+            total_sum += key * value
+        return total_sum
+
+    def withdraw(self, amount):
+        # Проверка возможности снять данную сумму
+        amount_tmp = amount
+        cash = {}
+        for key, value in self.__cash.items():
+            count = amount_tmp // key
+            if 0 < count <= value:
+                amount_tmp -= key * count
+                cash[key] = count
+        # Если можем снять такую сумму
+        if amount_tmp == 0:
+            # Извлекаем из банкомата необходимые купюры
+            for key, value in cash.items():
+                self.__cash[key] -= value
+            return cash
+        else:
+            raise Exception('Банкомат не может выдать данную сумму')
+
+    # Словарь -> str
+
+    # Загрузка денег в банкомат
+
+
+atm = ATM(4568)
+print(atm.total_amount())
+print(atm.withdraw(5050))
+print(atm.total_amount())
+print(atm)
--- 11. ООП/Задачи/Дата/Дата.py
+++ 11. ООП/Задачи/Дата/Дата.py
@@ -1,63 +1,49 @@
 class Date:
-    def __init__(self, date):
+    def __init__(self, date: str):
         lst = date.split('.')
         self.day = int(lst[0])
         self.month = int(lst[1])
         self.year = int(lst[2])
 
+    def get_next_day(self):
+        a = (1 - (self.year % 4 + 2) % (self.year % 4 + 1))
+        b = ((self.year % 100 + 2) % (self.year % 100 + 1))
+        c = (1 - (self.year % 400 + 2) % (self.year % 400 + 1))
+        return (28 + ((self.day + (self.day / 8)) % 2) + 2 % self.day + ((1 + a * b + c) / self.day) +
+                (1 / self.day) - ((a * b + c) / self.day))
+
     def add_day(self):
-        if self.year % 4 == 0 and self.year % 100 != 0 or self.year % 400 == 0:
-            if self.month == 2:
-                self.day += 1
-                if self.day > 29:
-                    self.day = 1
-                    self.month += 1
-            elif self.month == 12:
-                self.day += 1
-                if self.day > 31:
-                    self.day = 1
-                    self.month = 1
-                    self.year += 1
-            elif self.month == 4 or self.month == 6 or self.month == 9 or self.month == 11:
-                self.day += 1
-                if self.day > 30:
-                    self.day = 1
-                    self.month += 1
-            else:
-                self.day += 1
-                if self.day > 31:
-                    self.day = 1
-                    self.month += 1
-
-
+        self.day += 1
+        if (self.year % 4 == 0 and self.year % 100 != 0) or (self.year % 400 == 0):
+            if self.month == 2 and self.day == 29:
+                self.day = 1
+                self.month += 1
         else:
-            if self.month == 2:
-                self.day += 1
-                if self.day > 28:
-                    self.day = 1
-                    self.month += 1
-            elif self.month == 12:
-                self.day += 1
-                if self.day > 31:
-                    self.day = 1
-                    self.month = 1
-                    self.year += 1
-            elif self.month == 4 or self.month == 6 or self.month == 9 or self.month == 11:
-                self.day += 1
+            if self.month == 4 or self.month == 6 or self.month == 9 or self.month == 11:
                 if self.day > 30:
                     self.day = 1
                     self.month += 1
             else:
-                self.day += 1
                 if self.day > 31:
                     self.day = 1
                     self.month += 1
-
+        if self.month > 12:
+            self.month = 1
+            self.year += 1
 
     def __str__(self):
         return f'{self.day}.{self.month}.{self.year}'
 
 
-benya = Date('30.6.2000')
-benya.add_day()
-print(benya)
+from datetime import date, timedelta, datetime
+
+start_date = datetime(1700, 2, 28)
+for day in range(100000):
+    current_date = start_date + timedelta(days=day)
+    next_date = start_date + timedelta(days=day+1)
+    date = Date(current_date.strftime('%d.%m.%Y'))
+    date.add_day()
+    str_date = str(date)
+    if Date(next_date.strftime('%d.%m.%Y')).__str__() != str_date:
+        print('ERROR')
+        print(current_date)
